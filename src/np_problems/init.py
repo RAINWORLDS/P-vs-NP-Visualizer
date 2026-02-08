33952c5c927a5466d55856b7de001dcbf593c291
Pa2a11aximum <tecguan8@hotmail.com>
2026-02-08 10:55:53 +0800

    Create init.py

---
+++ src/np_problems/init.py
@@ -0,0 +1,13 @@
+from .traveling_salesman import TravelingSalesman
+from .sat_solver import SATSolver
+from .knapsack import KnapsackSolver
+from .subset_sum import SubsetSum
+from .graph_coloring import GraphColoring
+
+__all__ = [
+    'TravelingSalesman',
+    'SATSolver', 
+    'KnapsackSolver',
+    'SubsetSum',
+    'GraphColoring'
+]
